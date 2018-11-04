8d4f9dfa10f260ab4fba0c677614cf1a78e412a8
Tiffany Carter <tafby88@gmail.com>
2018-11-04 22:01:32 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,9 @@ def monopoly_with_third_tier
     :pieces => 4,
     :names => {
       :reading_railroad => {"mortgage_value" => "$200"},
-      :pennsylvaia_railroad => {},
-      :b_and_o_railroad => {},
-      :shortline => {}
+      :pennsylvaia_railroad => {"mortgage_value" => "$200"},
+      :b_and_o_railroad => {"mortgage_value" => "$400"},
+      :shortline => {"mortgage_value" => "$800"}
     },
     :rent_in_dollars => {
       :one_piece_owned => 25,
