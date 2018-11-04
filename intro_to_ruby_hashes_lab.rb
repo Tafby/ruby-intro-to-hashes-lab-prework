7ae9c19d2dd226cdea9f32e2a632486c8e0deab4
Tiffany Carter <tafby88@gmail.com>
2018-11-04 21:47:04 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,12 +17,11 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads =>
-	  {
-	    :pieces => 4
-	    :names => 
-	    :rent_in_dollars =>
-	  }
+	  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
 	}
 	
 end
